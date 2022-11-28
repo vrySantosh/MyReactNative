de106994ca267637569964384106c0ba7bbb3c58
Santosh Guruju <santosh.guruju@icloud.com>
2022-11-28 15:21:02 +0530

    Create PushNotifications.js

---
+++ PushNotifications.js
@@ -0,0 +1,32 @@
+/** Push notifications in React
+ 
+    Notifee
+    react-native-notification by wix
+    
+    react-native-notification by WIX
+        iOS:
+            Remote (push) notifications
+            Local notifications
+            Background/Managed notifications (notifications that can be cleared from the server, like Facebook messenger and Whatsapp web)
+            PushKit API (for VoIP and other background messages)
+            Interactive notifications (allows you to provide additional functionality to your users outside of your application such as action buttons)
+        Android:
+            Receiving notifications in any App state (foreground, background, "dead")
+            Built-in notification drawer management
+            High degree of code extensibility to allow for advanced custom layouts and any specific notifications behavior as available by Android's API
+            Android equivalent of React-Native's implementation of PushNotificationsIOS.getInitialNotification().
+            Upcoming: local notifications, background-state Rx queue (iOS equivalent)
+
+        Steps:
+            Install package.
+            Push notification subscription
+            Handling notifications events.(Foreground & background)
+            Querying intial notifications.
+            Local notifications / Canceling scheduled notifications.
+            iOS manual installation is also possible.
+            Android notificatios with google's FCM 
+            Advanced iOS APIs like replying to messages via notification.(PushKit)
+
+
+ */
+
